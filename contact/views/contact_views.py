e7546b42fd13b962614e2fed1a5260bb03f55188
FamousLuisin <lufimelo@outlook.com>
2024-01-29 22:10:59 -0300

    Criado a view de contato unico

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact import models
+from django.http import Http404
 
 # Create your views here.
 def index(request):
-    contacts = models.Contact.objects.all()
+    contacts = models.Contact.objects.filter(show=True).order_by('-id')
+
     context = {
         'contacts': contacts,
     }
@@ -12,5 +14,23 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = models.Contact.objects.filter(pk=contact_id).first()
+    # if single_contact is None:
+    #     raise Http404()
+
+    single_contact = get_object_or_404(models.Contact, pk=contact_id, show=True)
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
