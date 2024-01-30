480d924055790ff49956b80b1280cf4caa6de9c5
FamousLuisin <lufimelo@outlook.com>
2024-01-30 11:29:23 -0300

    programação do search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,46 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact import models
 from django.http import Http404
+from django.db.models import Q
+
 
 # Create your views here.
 def index(request):
-    contacts = models.Contact.objects.filter(show=True).order_by('-id')
+    contacts = models.Contact.objects.filter(show=True).order_by('-id')[0:10]
 
     context = {
         'contacts': contacts,
         'site_title': 'contatos - ',
+    }
+
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    # Esse Q, permite usar o | para ser usado como or
+    contacts = models.Contact.objects.filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'contatos - ',
+        'search_value': search_value,
     }
 
 
